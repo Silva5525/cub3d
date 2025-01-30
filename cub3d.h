b0fc403d8120d46b5adf1ed84949f47bd54b137d
matvej <mgering@mail.de>
2025-01-30 16:24:24 +0100

    2D map rendering

--- cub3d.h
+++ cub3d.h
@@ -21,26 +21,30 @@
 # include <stdbool.h>
 # include <unistd.h>
 
-# define WIDTH 512
-# define HEIGHT 512
+# define WIDTH 2048
+# define HEIGHT 2048
+# define TILE_SIZE 32
+# define PLAYER_SIZE 16
+# define PI 3.141592
 
 typedef struct s_texture
 {
 	mlx_image_t		*img;
-	int				width;
+	int				width; 
 	int				height;
 }					t_texture;
 
 typedef struct s_vector
 {
-	double			x;
-	double			y;
+	float			player_x;
+	float			player_y;
 }					t_vector;
 
 typedef struct s_char
 {
+	mlx_image_t		*player_img;
 	t_vector		pos;
-	t_vector		dir;
+	float			angle;
 	t_vector		plane;
 }					t_char;
 
@@ -64,5 +68,8 @@ bool	parse_line(char *trim_line, t_c *cub, mlx_texture_t *texture,
 bool	valid_map(t_c *cub, int p_count, int x, int y);
 void	create_player(t_c *cub, int x, int y);
 bool	map_err(char *line, int fd);
+
+//draw_map.c
+void draw_map2D(t_c *cub);
 
 #endif
